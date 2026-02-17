7f70488a179715a439faf2cff055c6aa13476ab6
MoeedThePessimist <moeedawan2121@gmail.com>
2026-02-17 23:48:41 +0500

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import CustomCursor from './utils/CursorAnimation'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import CustomCursor from "./utils/CursorAnimation";
 
 export default function App() {
   return (
-    <div className='font-sora scroll-smooth overflow-x-hidden'>
-      <CustomCursor/>
+    <div className="font-sora scroll-smooth overflow-x-hidden">
+      <CustomCursor />
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -15,5 +15,5 @@ export default function App() {
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
