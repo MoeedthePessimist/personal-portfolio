49af3ab3601e24a6680e7fa350dda0b809491936
MoeedThePessimist <moeedawan2121@gmail.com>
2026-02-19 08:41:50 +0500

    exported all the animations

---
+++ src/components/animations/index.js
@@ -0,0 +1,6 @@
+// src/components/animations/index.js
+// Central export for all React Bits-style animations
+export { default as Aurora } from "./Aurora";
+export { default as Particles } from "./Particles";
+export { default as GridPattern } from "./GridPattern";
+export { default as Starfield } from "./Starfield";
