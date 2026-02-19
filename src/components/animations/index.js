0fd8d8f709f9af20a4a96c9036dae0ca62fbe538
MoeedThePessimist <moeedawan2121@gmail.com>
2026-02-19 10:01:57 +0500

    added galaxy background animation

--- src/components/animations/index.js
+++ src/components/animations/index.js
@@ -1,7 +1,3 @@
 // src/components/animations/index.js
 // Central export for all React Bits-style animations
-export { default as Aurora } from "./Aurora";
-export { default as Particles } from "./Particles";
-export { default as GridPattern } from "./GridPattern";
-export { default as Starfield } from "./Starfield";
-export { default as FloatingLines } from "./FloatingLines";
+export { default as Galaxy } from "./Galaxy";
