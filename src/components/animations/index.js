dd7fbd81fcef0861abe8b51b8e2a138f6667d167
MoeedThePessimist <moeedawan2121@gmail.com>
2026-03-01 07:54:40 +0500

    chore: update three.js to version 0.183.1, add LiquidEther animation component, and integrate it into ProjectsPage

--- src/components/animations/index.js
+++ src/components/animations/index.js
@@ -4,3 +4,5 @@ export { default as Galaxy } from "./Galaxy";
 export { default as RotatingText } from "./RotatingText";
 export { default as ProfileCard } from "./ProfileCard";
 export { default as CircularText } from "./CircularText";
+export { default as GlareHover } from "./GlareHover";
+export { default as LiquidEther } from "./LiquidEther";
